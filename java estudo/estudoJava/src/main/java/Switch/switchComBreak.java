5e7f8716b0117abcd769adc8df4e0a525433b918
ThiagoArica <thiago.arica@mercadolivre.com>
2023-01-24 16:03:14 -0300

    efetuado estudo do SWITCH.Com Break

---
+++ java estudo/estudoJava/src/main/java/Switch/switchComBreak.java
@@ -0,0 +1,10 @@
+package Switch;
+
+import java.util.Scanner;
+
+public class switchComBreak {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        
+    }
+}
