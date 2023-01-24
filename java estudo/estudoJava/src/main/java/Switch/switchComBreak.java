f76b9d01aa76f00b33843f025006d0667bcac999
ThiagoArica <thiago.arica@mercadolivre.com>
2023-01-24 16:46:43 -0300

    efetuado estudo do break 1

--- java estudo/estudoJava/src/main/java/Switch/switchComBreak.java
+++ java estudo/estudoJava/src/main/java/Switch/switchComBreak.java
@@ -5,6 +5,37 @@ import java.util.Scanner;
 public class switchComBreak {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        
+        String conceito = "";
+        System.out.println("informe a  sua nota ");
+        int nota = read.nextInt();
+        switch (nota) {
+            case 10:
+            case 9:
+                conceito = "A";
+                break;
+            case 7:
+            case 8:
+                conceito = "b";
+                break;
+            case 6:
+            case 5:
+                conceito = "b";
+                break;
+            case 4:
+            case 3:
+                conceito = "c";
+                break;
+            case 2:
+            case 1:
+                conceito = "b";
+                break;
+            case 0:
+                conceito = "reprovado";
+                break;
+            default:
+                conceito = "nao informado";
+        }
+        System.out.println("conceito é " + conceito);
+        read.close();
     }
 }
