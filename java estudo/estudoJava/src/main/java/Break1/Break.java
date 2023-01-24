e398ce2f155a2ce968a8dfd54f9d363698a629cf
ThiagoArica <thiago.arica@mercadolivre.com>
2023-01-24 17:08:59 -0300

    efetuado estudo do continue

---
+++ java estudo/estudoJava/src/main/java/Break1/Break.java
@@ -0,0 +1,17 @@
+package Break1;
+
+import java.util.Scanner;
+
+public class Break {
+    public static void main(String[] args) {
+        Scanner read= new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            if (i == 5){
+                break;
+}
+
+        }
+        System.out.println("fim ");
+    }
+
+}
---
+++ java estudo/estudoJava/src/main/java/Continue/Continue.java
@@ -0,0 +1,5 @@
+package Continue;
+
+public class Continue {
+    
+}
