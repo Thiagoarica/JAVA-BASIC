635f44f26361ebf62073ceb5ce90908d1b060f0e
ThiagoArica <thiago.arica@mercadolivre.com>
2023-05-05 23:23:22 -0300

    orientacao _obejetos_ encapsu;lamento e suas regras

---
+++ java estudo/estudoJava/src/main/java/oo/encapsulamento/casaA/Miguel.java
@@ -0,0 +1,13 @@
+package oo.encapsulamento.casaA;
+
+public class Miguel {
+    void testeAcesso(){
+        Thiago pai =new Thiago();
+        //SEGREDO= "....";
+        //dentroDeCasa = "......";
+          //FORMAdEFalar="....";
+         //Liberado= "....";
+        System.out.println(pai.dentroDeCasa );
+
+    }
+}
---
+++ java estudo/estudoJava/src/main/java/oo/encapsulamento/casaA/Thiago.java
@@ -0,0 +1,10 @@
+package oo.encapsulamento.casaA;
+
+public class Thiago {
+    @SuppressWarnings("unused")
+    private String SEGREDO= "....";
+    String dentroDeCasa = "......";
+    protected String FORMAdEFalar="....";
+    public String Liberado= "....";
+
+}
